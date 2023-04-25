32d3778d4cedf7517e3b8d23f9111cd4b4811884
riddhiparmar48 <riddhi.parmar@aglowiditsolutions.com>
2023-04-25 19:23:43 +0530

    started card flip

--- src/CardFlip.js
+++ src/CardFlip.js
@@ -1,6 +1,44 @@
-import React from "react";
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import "./App.css";
+import { Box, Grid } from "@mui/material";
 
 export default function CardFlip() {
-  return <div>CardFlip</div>;
+  const [flipCard, setFlipCard] = useState(true);
+
+  // const handleClick = (event) => {
+  //   setFlipCard((current) => !current);
+  // };
+
+  // <button className={this.state.active && 'active'}
+  //     onClick={ () => this.setState({active: !this.state.active}) }>Click me</button>
+
+  return (
+    <>
+      <Navbar />
+      <Box>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <div className="scene scene--card">
+              <div className="card">
+                <div
+                  className={flipCard ? "card__face card__face--front" : ""}
+                  // onClick={handleClick}
+                >
+                  front
+                </div>
+                <div
+                  className={flipCard ? "card__face card__face--back" : ""}
+                  // onClick={handleClick}
+                >
+                  back
+                </div>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
 }
-    
