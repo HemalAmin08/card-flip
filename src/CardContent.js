ae7d15c24b0f1cf5f80f7507db1971888dba2af9
riddhiparmar48 <riddhi.parmar@aglowiditsolutions.com>
2023-04-27 12:43:59 +0530

    api call done card static array and map done, onclick card flip remaining

---
+++ src/CardContent.js
@@ -0,0 +1,8 @@
+const cardContent = [
+  { id: 1, name: "Pokemon 1" },
+  { id: 2, name: "Pokemon 2" },
+  { id: 3, name: "Pokemon 3" },
+  { id: 4, name: "Pokemon 4" },
+  { id: 5, name: "Pokemon 5" },
+];
+export default cardContent;
--- src/CardFlip.js
+++ src/CardFlip.js
@@ -1,39 +1,69 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import "./App.css";
 import { Box, Grid } from "@mui/material";
+import cardContent from "./CardContent";
 
 export default function CardFlip() {
   const [flipCard, setFlipCard] = useState(true);
+  const [cardContentData, setCardContentData] = useState([]);
+  const [cardContentItem, setCardContentItem] = useState(cardContent);
 
-  const handleClick = (event) => {
-    setFlipCard(!flipCard);
+  const handleClick = (id) => {
+    // console.log(id, "id");
+    cardContentItem.forEach((e) => {
+      // console.log(e.id === id, "e map");
+      if (e.id === id) {
+        setFlipCard(!flipCard);
+      }
+    });
   };
+  const handleCardContentData = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((json) => {
+        setCardContentData(json);
+      });
+  };
+
+  useEffect(() => {
+    handleCardContentData();
+  }, []);
+
+  // console.log(cardContentData.results, "cardContentData"); // 20 pokemon data
 
   return (
     <>
       <Navbar />
       <Box>
         <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <div className="scene scene--card">
-              <div
-                // className="card"
-                className={!flipCard ? "card " : "card is-flipped"}
-                onClick={handleClick}
-              >
-                <div className={flipCard ? "card__face card__face--front" : ""}>
-                  front
+          <Grid item x1={4}>
+            {cardContent.map(({ name, id }) => {
+              return (
+                <div className="scene scene--card" key={id}>
+                  <div
+                    className={!flipCard ? "card  is-flipped" : "card"}
+                    onClick={() => {
+                      handleClick(id);
+                    }}
+                  >
+                    <div
+                      className={
+                        flipCard
+                          ? "card__face card__face--back"
+                          : "card__face--back"
+                      }
+                    >
+                      {name}
+                    </div>
+                    <div className="card__face card__face--front">front</div>
+                  </div>
                 </div>
-                <div
-                  className={"card__face card__face--back"}
-                  // className="card__face card__face--back"
-                >
-                  back
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </Grid>
         </Grid>
       </Box>
